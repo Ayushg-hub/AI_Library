098d28de8b95d3fd6fd7a6dcaffffd8722e9d79f
ayushdp325@gmail.com <ayushdp325@gmail.com>
2021-08-10 06:43:13 -0400

    Add project files.

---
+++ AI_Library.cpp
@@ -0,0 +1,12 @@
+﻿// AI_Library.cpp : Defines the entry point for the application.
+//
+
+#include "AI_Library.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
